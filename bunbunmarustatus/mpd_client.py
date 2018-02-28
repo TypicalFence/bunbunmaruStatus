244b5384e76b315bd4a1cc6aaf0b6bb57f476dc0
Alex Fence <alexfence.code@gmail.com>
2018-02-28 17:03:04 +0100

    refactored MPD module

--- bunbunmarustatus/mpd_client.py
+++ bunbunmarustatus/mpd_client.py
@@ -1,22 +1,37 @@
 from mpd import MPDClient
 from .module import Module
 
-client = MPDClient()
-
-def get_current_song():
-    try:
-        client.connect("localhost", 6600)
-        current = client.currentsong()
-        client.close()
-        client.disconnect()
-        return current["title"] + " - " + current["artist"]
-
-    except:
-        return ""
 
 class MPD(Module):
+    def __init__(self):
+        self._client = MPDClient()
+
     def get_name(self):
         return "mpd"
     
     def get_block(self):
-        return {"full_text": get_current_song() }
+        return {"full_text": self._get_current_song() }
+    
+    def _get_song_string(self):
+        current = self._client.currentsong()
+        return current["title"] + " - " + current["artist"]
+
+    def _get_current_song(self):    
+        try:
+            self._client.connect("localhost", 6600)
+
+            state = self._client.status()["state"]
+            output = self._get_song_string()
+
+            self._client.close()
+            self._client.disconnect()
+
+            if state == "play":
+                return output
+            elif state == "pause":
+                return "*" + output + "*"
+            elif state == "stop": 
+                return ""
+
+        except:
+            return ""

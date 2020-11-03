2a398dea6ac3dbdf3cc59d5fce24dff376492bbf
fence <fence@desu-mail.moe>
2020-11-03 13:48:09 +0100

    pulseaudio: use default sink

--- bunbunmarustatus/pulse.py
+++ bunbunmarustatus/pulse.py
@@ -6,9 +6,25 @@ class PulseAudio(Module):
     def __init__(self):
         super().__init__()
         self.pulse = pulsectl.Pulse()
+        self.sink_name = self.pulse.server_info().default_sink_name
+
+    def _locate_default_sink(self):
+        sinks = self.pulse.sink_list()
+        results = filter(lambda x: x.name == self.sink_name, sinks)
+        # filter objects in python are annoying
+        results = list(results)
+
+        if len(results) != 0:
+            return results[0]
+
+        return None
 
     def get_block(self):
-        sink = self.pulse.sink_list()[0]
+        sink = self._locate_default_sink()
+
+        if sink is None:
+            return {"full_text": "no audio"}
+
         volume = sink.volume.value_flat * 100
         if sink.mute == 0:
             text = str(volume).split(".")[0] + "%"
